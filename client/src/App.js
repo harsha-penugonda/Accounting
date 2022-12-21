9357c0f0a3e07dc0d4c8dc299c84c3f1202d916b
harsha-penugonda <vpenugon@stevens.edu>
2022-12-21 16:57:20 -0500

    react client commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Accounting App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
